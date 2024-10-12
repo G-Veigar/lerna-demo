fcb40d2760524c53c1d7d3de9c2229b27f8a6e6f
高歌 <ge.gao@lkcoffee.com>
2024-10-12 16:30:34 +0800

    init3

--- packages/module1/lib/module1.js
+++ packages/module1/lib/module1.js
@@ -1,5 +1,18 @@
-const utils = require('@lk/utils')
+const fs = require('fs');
+const path = require('path');
 
-utils()
+function createFile() {
+    // 使用 path.resolve() 定位到项目根目录的 dist 文件夹
+    const filePath = path.resolve(__dirname, '../dist/hello-lerna.txt'); 
+    const content = 'hello lerna';
 
-console.log('module 1')
+    fs.writeFile(filePath, content, (err) => {
+        if (err) {
+            return console.error(`Error writing file: ${err.message}`);
+        }
+        console.log('File created and content written successfully in the dist folder!');
+    });
+}
+
+// 调用函数生成文件
+createFile();
